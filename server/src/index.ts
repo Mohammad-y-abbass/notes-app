3446003126ea6e88b394e46f91cba8a2e91206ca
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2025-01-02 16:04:45 +0200

    setup path alias

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,17 @@
 import express, { Express, Request, Response } from 'express';
 
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app: Express = express();
 
-const port = 8080;
+const PORT = process.env.PORT || 9001;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
